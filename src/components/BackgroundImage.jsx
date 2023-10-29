c37578220eb1efc1f3345f085696f21ac0da454c
Camilo Escobar Stange <escobar.stange@gmail.com>
2023-10-29 20:51:43 -0300

    asdasdasd

--- src/components/BackgroundImage.jsx
+++ src/components/BackgroundImage.jsx
@@ -1,7 +1,12 @@
 import React from "react";
-
+import background from "../asset/login.jpg"
+import styled from "styled-components";
 export default function BackgroundImage(){
     return (
-        <div>BackgroundImage</div>
-    )
+        <Container>
+            <img src={background} alt="background"/>
+        </Container>
+    );
 }
+
+const Container = styled.div``;
