68b346d2d5534030ea4261327b673ddd23c089c4
Camilo Escobar Stange <escobar.stange@gmail.com>
2023-10-29 21:20:33 -0300

    asdasdasd

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import styled from "styled-components";
+import logo from "../asset/logo.png"
+import { useNavigate } from "react-router-dom";
+export default function Header(props){
+        const navigate=useNavigate
+    return (
+        <Container>
+            <div className="logo">
+                <img src={logo} alt="logo" />
+            </div>
+            <button onClick={()=>navigate(props.login ?"/Iniciar Sesion" : "Registrase")}>{props.login ? "Iniciar Sesion" : "Registrarse"}
+            </button>        
+        </Container>
+    );
+}
 
-export default function Header(){
-    return <div>Header</div>
-} 
+const Container =styled.div``;
